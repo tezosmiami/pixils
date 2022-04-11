3ad0a18041ea35252733b46204f1fbda8448bcbb
tezosmiami <tezosmiami@gmail.com>
2022-04-11 12:16:55 -0400

    filter h=n banned. . .

--- src/components/latest-sales.js
+++ src/components/latest-sales.js
@@ -10,6 +10,7 @@ export const getLatestSales = gql`
       artifact_uri
       fa2_address
       token_id
+      artist_address
     }
    }  
    ` 
@@ -37,10 +38,13 @@ export const LatestSales = () => {
   const [offset, setOffset] = useState(Math.floor(Math.random() * 49892))
   const fetcher = (key, query, offset) => request(process.env.REACT_APP_TEZTOK_API, query, {offset})
   const { data, error} = useSWR(['/api/sales', getLatestSales, offset], fetcher, { refreshInterval: 5000 })
-console.log(data?.tokens.length)
+
   if (error) return <p>Error</p>
   if (!data) return <p>Loading. . .</p>
- 
+
+  const axios = require('axios');
+  const banned = async() => await axios.get('https://raw.githubusercontent.com/hicetnunc2000/hicetnunc-reports/main/filters/w.json');
+  const final = data?.tokens.filter((i) => !banned?.data?.includes(i.artist_address))
 
 
 //   totalpixils?.length > 0 && totalpixils.sort(function (a, b) {
@@ -50,11 +54,11 @@ console.log(data?.tokens.length)
     return (
       <>
       <div className='container'>
-        {data && data.tokens.map(p=> (
+        {final && final.map(p=> (
           // p.mime_type !== null &&
           // p.eightbid_rgb === null &&
           p.mime_type.includes('image') && p.mime_type !== 'image/svg+xml' ? 
-           <a key={p.token_id} href={p.fa2_address ==='KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton' ? `https://hicetnunc.miami/objkt/${p.token_id}` : 
+           <a key={p.artifact_uri} href={p.fa2_address ==='KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton' ? `https://hicetnunc.miami/objkt/${p.token_id}` : 
               p.fa2_address === 'KT1LjmAdYQCLBjwv4S2oFkEzyHVkomAf5MrW' ? `https://versum.xyz/token/versum/${p.token_id}` 
 
              : `https://objkt.com/asset/${p.fa2_address}/${p.token_id}`} target="blank"  rel="noopener noreferrer">  
