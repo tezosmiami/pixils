464929bb6eac73d13c610552a0778e542f277d46
tezosmiami <tezosmiami@gmail.com>
2022-04-11 14:13:15 -0400

    video + random offset by count. . .

--- src/components/latest-sales.js
+++ src/components/latest-sales.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { request, gql } from 'graphql-request'
 import useSWR, { useSWRConfig } from 'swr';
+import ReactPlayer from 'react-player'
 
-
+export const getTotalObjkts = gql`
+  query getTotal{
+    tokens_aggregate(where: {editions: {_eq: "1"}, price: {_is_null: false}, mime_type: {_is_null: false}}) {
+    aggregate {
+      count
+    }
+  }
+}
+`
 export const getLatestSales = gql`
   query getSales ($offset: Int!) {
     tokens(where: {editions: {_eq: "1"}, price: {_is_null: false}, mime_type: {_is_null: false}}, offset: $offset, limit: 108) {
@@ -14,6 +23,7 @@ export const getLatestSales = gql`
     }
    }  
    ` 
+const fetcher = (key, query, offset) => request(process.env.REACT_APP_TEZTOK_API, query, {offset})
 
 export function sliceChunks(arr, chunkSize) {
   const res = [];
@@ -35,18 +45,39 @@ export function sliceChunks(arr, chunkSize) {
 export const LatestSales = () => {
   const { mutate } = useSWRConfig()
   const [pageIndex, setPageIndex] = useState(0);
-  const [offset, setOffset] = useState(Math.floor(Math.random() * 49892))
-  const fetcher = (key, query, offset) => request(process.env.REACT_APP_TEZTOK_API, query, {offset})
-  const { data, error} = useSWR(['/api/sales', getLatestSales, offset], fetcher, { refreshInterval: 5000 })
+  const [offset, setOffset] = useState(0)
+  const [banned, setBanned] = useState()
+
+  const axios = require('axios');
+
+  useEffect(() => {
+    const getTotal = async () => {
+      const result = await request(process.env.REACT_APP_TEZTOK_API, getTotalObjkts)
+      setOffset(Math.floor(Math.floor(Math.random() * result.tokens_aggregate.aggregate.count)))
+  }
+    getTotal();
+  }, [])
+
+  useEffect(() => {
+    const getBanned = async () => {
+    const result = await axios.get('https://raw.githubusercontent.com/hicetnunc2000/hicetnunc-reports/main/filters/w.json') ;
+    setBanned(result.data)
+  }
+    getBanned();
+  }, [])
+  
+  
+  // const { data: total, error: totalerror} = useSWR([`/api/total`, getTotalObjkts, null], fetcher,{ refreshInterval: 5000 })
+  // setOffset(total?.tokens_aggregate.aggregate.count-108)
+  const { data, error } = useSWR(offset>0 && ['/api/sales', getLatestSales, offset], fetcher, { refreshInterval: 5000 })
 
   if (error) return <p>Error</p>
   if (!data) return <p>Loading. . .</p>
 
-  const axios = require('axios');
-  const banned = async() => await axios.get('https://raw.githubusercontent.com/hicetnunc2000/hicetnunc-reports/main/filters/w.json');
-  const final = data?.tokens.filter((i) => !banned?.data?.includes(i.artist_address))
-
-
+  const final = data?.tokens.filter((i) => !banned.includes(i.artist_address))
+console.log(offset)
+console.log(data)
+console.log(final)
 //   totalpixils?.length > 0 && totalpixils.sort(function (a, b) {
 //     return b.opid - a.opid;
 //   });
@@ -54,7 +85,7 @@ export const LatestSales = () => {
     return (
       <>
       <div className='container'>
-        {final && final.map(p=> (
+        {final && final.slice(0,108).map(p=> (
           // p.mime_type !== null &&
           // p.eightbid_rgb === null &&
           p.mime_type.includes('image') && p.mime_type !== 'image/svg+xml' ? 
@@ -66,10 +97,15 @@ export const LatestSales = () => {
           </a>
            :
           // p.token.mime_type !== null &&
-          // p.token.mime_type.includes('video') ?  
-        
-          // <video className='pop video' key={p.opid}  src={'https://cloudflare-ipfs.com/ipfs/' + p.token.artifact_uri.slice(7)} /> 
-        
+          p.mime_type.includes('video') ?  
+          <a key={p.artifact_uri} href={p.fa2_address ==='KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton' ? `https://hicetnunc.miami/objkt/${p.token_id}` : 
+          p.fa2_address === 'KT1LjmAdYQCLBjwv4S2oFkEzyHVkomAf5MrW' ? `https://versum.xyz/token/versum/${p.token_id}` 
+
+         : `https://objkt.com/asset/${p.fa2_address}/${p.token_id}`} target="blank"  rel="noopener noreferrer">  
+           <div className='pop video'>
+             <ReactPlayer url={'https://ipfs.io/ipfs/' + p.artifact_uri.slice(7)} width='100%' height='100%' muted={true} playing={true} loop={true}/>
+            </div>
+            </a>
           // :
           // p.eightbid_rgb !== null ?
           //   //  <a key={p.opid} href={`https://www.8bidou.com/listing/?id=${p.token.token_id}`} target="blank"  rel="noopener noreferrer">
@@ -84,7 +120,7 @@ export const LatestSales = () => {
           //     </div>
           //     </a>
           //    :
-           null        
+           : null        
             ))}
        <div>
           <p></p>
